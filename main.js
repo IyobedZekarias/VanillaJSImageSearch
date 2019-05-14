25299c63fdcf4e6a1ff7f5a5e10e6e2c0a78f3f1
IyobedZekarias <50155965+IyobedZekarias@users.noreply.github.com>
2019-05-14 02:07:44 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+let API_URL = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?';
+const form = document.querySelector('#inputs');
+const input = document.querySelector('#searchTerm');
+const loader = document.querySelector('#loadingimage');
+const images = document.querySelector('.images');
+form.addEventListener('submit', formSubmitted);
+
+loader.style.display = 'none';
+
+function formSubmitted(event){
+	event.preventDefault();
+	const searchTerm = input.value;
+
+	API_URL +='sol=' + searchTerm + '&page=1&api_key=aJDLkW5WTb3oYgmdZgpaaqru0fBvOoxWL08TKP2J';
+
+	search(searchTerm)
+		.then(displayImages);
+}
+
+function search(searchTerm){
+	loader.style.display = '';
+	return fetch(`${API_URL}`)
+		.then(response => response.json())
+		.then(result =>{
+			console.log(result.photos);
+			return result.photos;
+		});
+}
+
+function displayImages(Images){
+	Images.forEach(image => {
+		const imageElement = document.createElement('img');
+		imageElement.style.width = '400px';
+		imageElement.style.height = '400px';
+		imageElement.src = image.img_src;
+		images.appendChild(imageElement);
+	})
+	loader.style.display = 'none';
+}
+
+//sol=1000&page=1&api_key=aJDLkW5WTb3oYgmdZgpaaqru0fBvOoxWL08TKP2J';
